40272d5a7d82837bef6cc0ac0499e3d49879e9f3
Rodrigo Sellanes <rs08@ucema.edu.ar>
2022-10-16 10:56:34 +0200

    first push of the project structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <Header></Header>
+
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "HomeView",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
