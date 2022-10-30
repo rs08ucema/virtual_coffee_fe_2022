e4ff03688b464cd383889942af6d0030184e79c6
Rodrigo Sellanes <rs08@ucema.edu.ar>
2022-10-30 11:16:04 +0100

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import * as Vue from 'vue';
+// Official documentation -> https://router.vuejs.org/guide/#html
+import * as  VueRouter from 'vue-router';
 
-createApp(App).mount('#app')
+// Import defined Views
+import HomeView from "@/views/HomeView";
+import App from "@/App";
+// import NotFound from "@/views/NotFound";
+
+
+// Here we will define all the routes
+const routes = [
+    {path: '/', component: HomeView},
+
+    // { path: '*', component: NotFound }
+]
+
+// Create the object router, add pass the routes above
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+// Add router to the Vue instance
+Vue.createApp(App)
+    .use(router)
+    .mount('#app');
---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header/>
+    <br>
+    <h1>No encontramos la página :'(</h1>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "NotFound",
+  components: {Header}
+}
+</script>
+
+<style scoped>
+
+</style>
